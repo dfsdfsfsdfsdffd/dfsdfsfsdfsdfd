09bb24e567e5cab76646586ba5c6db9a344bcff2
dfsdfsfsdfsdffd <dyes401@gmail.com>
2025-07-24 22:55:45 -0400

    Add files via upload

--- ref/fuckyougang.js
+++ ref/fuckyougang.js
@@ -1,14 +1,13 @@
-(function () {
+(function() {
   const threshold = 160;
 
   const detectDevTools = () => {
     const widthThreshold = window.outerWidth - window.innerWidth > threshold;
     const heightThreshold = window.outerHeight - window.innerHeight > threshold;
-    const isDevtoolsOpen = widthThreshold || heightThreshold;
+    const isDevToolsOpen = widthThreshold || heightThreshold;
 
-    if (isDevtoolsOpen) {
-      
-      window.location.href = "https://www.pornhub.com/view_video.php?viewkey=68705453d9fe8";
+    if (isDevToolsOpen) {
+      window.location.href = "https://www.pornhub.com/view_video.php?viewkey=68705453d9fe8"; 
     }
   };
 
