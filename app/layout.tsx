60a66ac2c5770d1bd90cd96ad02a09cfdf915801
dfsdfsfsdfsdffd <dyes401@gmail.com>
2026-03-16 00:15:38 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import './globals.css'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
