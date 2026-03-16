76f173c495a6d96c817fce4e10b84cfc26f284cd
dfsdfsfsdfsdffd <dyes401@gmail.com>
2026-03-16 02:28:48 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "softcard.cc",
+  description: "Claim your link",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-black antialiased">
+        {children}
+      </body>
+    </html>
+  );
+}
